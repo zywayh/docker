60bbc4917c920c053c042e26db17ebf04d922169
zywayh <865308221@qq.com>
2018-10-15 13:44:55 +0800

    Update install-alioss.sh

--- shell/install-alioss.sh
+++ shell/install-alioss.sh
@@ -5,14 +5,14 @@ if [ $# -ge 3 ];then
     yum install wget -y
     
     echo "下载ossfs镜像文件"
-    wget ossfs_rpm_path=http://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/assets/attach/32196/cn_zh/1507811577850/ossfs_1.80.5_centos7.0_x86_64.rpm
+    wget http://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/assets/attach/32196/cn_zh/1527232195135/ossfs_1.80.5_centos7.0_x86_64.rpm
     
     echo "安装ossfs镜像文件"
     sudo yum localinstall ossfs_1.80.5_centos7.0_x86_64.rpm
 
     echo "设置oss账号信息"
     #echo <my-bucket>:<my-access-key-id>:<my-access-key-secret> /etc/passwd-ossfs
-    echo $1:$2:$3 /etc/passwd-ossfs
+    echo $1:$2:$3 > /etc/passwd-ossfs
     chmod 640 /etc/passwd-ossfs
     
     echo "映射oss目录"
