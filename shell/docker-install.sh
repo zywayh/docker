dd472aa9558e8ab7eef8912b2a5e76c0c117c6b6
zywayh <865308221@qq.com>
2018-10-16 18:40:03 +0800

    Update docker-install.sh

--- shell/docker-install.sh
+++ shell/docker-install.sh
@@ -5,7 +5,16 @@ echo "docker linix contos7.* install init --------------------------------------
 systemctl start docker
 if [ $? -ne 0 ]; then 
   echo "安装阿里docker-----------------------------------------------------"
-  curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
+  # step 1: 安装必要的一些系统工具
+  sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+  # Step 2: 添加软件源信息
+  sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+  # Step 3: 更新并安装 Docker-CE
+  sudo yum makecache fast
+  sudo yum -y install docker-ce
+  # Step 4: 开启Docker服务
+  sudo service docker start
+
   echo "配置阿里docker镜像加速器-----------------------------------------------------------------"
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
