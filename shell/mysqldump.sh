3a9c541295cee5e10de0053ff3655158a6bca8f8
zywayh <865308221@qq.com>
2018-10-17 19:28:44 +0800

    Update mysqldump.sh

--- shell/mysqldump.sh
+++ shell/mysqldump.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-#host 要写外网域名,或者ip,或者能保证容器读取到的ip,不能写localhost
-#./mysqldump.sh <host> <port> <pass> <databaseName> <path>
+#定时任务   crontab -e
 
-if [ $# -ge 5 ];then
-    zcDATE=$(date +%Y%m%d%H%M%S)
-    databaseName=$4
-    savepath=$5/$databaseName-$zcDATE.sql
-    docker run --rm -ti 865308221/mysql:5.7.18 mysqldump -h$1 --port=$2 -uroot -p$3 --databases $databaseName > $savepath
-else
-    echo "参数错误 <host> <port> <pass> <database name> "
-fi
+#$1 为需要备份数据库的镜像名称,/dbbak/目录为镜像内部目录,需要映射到本地
+docker exec $1 sh -c '/usr/bin/mysqldump --set-gtid-purged=OFF -u root -pFlzx3000c --all-databases >/dbbak/all.sql'
+#镜像内部映射到本地的目录
+cd /dbbak
+zcDATE=$(date +%Y%m%d%H%M%S)
+databaseName=$3
+savepath=/dbbak/$zcDATE.sql.tgz
+#压缩文件
+tar czvf $savepath all.sql
+rm all.sql
+#将压缩过的文件转移到指定目录
+mv $savepath  /ossfs/mysql/$1/
