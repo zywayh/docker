d5ac11f5c98d260cb1a652114998248774aff25b
zywayh <865308221@qq.com>
2018-10-17 15:18:38 +0800

    Update mysqldump.sh

--- shell/mysqldump.sh
+++ shell/mysqldump.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ $# -ge 4 ];then
+if [ $# -ge 5 ];then
     zcDATE=$(date +%Y%m%d%H%M%S)
-    databaseName=$3
-    savepath=$4$databaseName-$zcDATE.sql
-    docker run --rm -ti mysql mysqldump -h $1 -uroot -p$2 --databases $databaseName > $savepath
+    databaseName=$4
+    savepath=$5/$databaseName-$zcDATE.sql
+    docker run --rm -ti 865308221/mysql:5.7.18 mysqldump -h$1 --port=$2 -uroot -p$3 --databases $databaseName > $savepath
 else
-    echo "参数错误 <host> <pass> <database name> "
+    echo "参数错误 <host> <port> <pass> <database name> "
 fi
