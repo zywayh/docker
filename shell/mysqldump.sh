4d6a441aa715306b85bffd9081f9391fd195d6d9
zywayh <865308221@qq.com>
2018-10-17 15:20:13 +0800

    Update mysqldump.sh

--- shell/mysqldump.sh
+++ shell/mysqldump.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#host 要写外网域名,或者ip,或者能保证容器读取到的ip,不能写localhost
+#./mysqldump.sh <host> <port> <pass> <databaseName> <path>
+
 if [ $# -ge 5 ];then
     zcDATE=$(date +%Y%m%d%H%M%S)
     databaseName=$4
