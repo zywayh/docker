fcc5b0a5eeec776cfb262e9889be0360fe1ddfac
朱亚伟 <zhuyawei@prospect-media.cn>
2019-06-24 16:37:47 +0800

    no message

--- shell/webhook.sh
+++ shell/webhook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-message="大家该喝水了，每小时200毫升水，让你的肾更健康，让你的容颜更美丽！"
+message="@所有人 大家该喝水了，每小时200毫升水，让你的肾更健康，让你的容颜更美丽！"
 URL=https://oapi.dingtalk.com/robot/send?access_token=a30323bc886c3c94713bbf606d986f8089d5232bdbe223c6818b974c54c8fcd9
 result=$(curl -H "Content-type: application/json" -X POST -s -w %{http_code} -d '{"msgtype": "text","text": {"content": "'${message}'"}}' ${URL})
 echo $result
