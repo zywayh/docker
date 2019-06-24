92ebc41f7fdf072ee678c7443d63f239dca17337
朱亚伟 <zhuyawei@prospect-media.cn>
2019-06-24 15:46:14 +0800

    no message

--- shell/webhook.sh
+++ shell/webhook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-message=$1
+message=$*
 URL=https://oapi.dingtalk.com/robot/send?access_token=a30323bc886c3c94713bbf606d986f8089d5232bdbe223c6818b974c54c8fcd9
 result=$(curl -H "Content-type: application/json" -X POST -s -w %{http_code} -d '{"msgtype": "text","text": {"content": "'${message}'"}}' ${URL})
 echo $result
