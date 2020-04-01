610325eccabe313700d01860b28598a4473d9d35
朱亚伟 <865308221@qq.com>
2020-04-01 23:02:28 +0800

    no message

--- shell/rc.sh
+++ shell/rc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# 开机执行脚本
-#echo $PWD/rc.sh >> /etc/rc.d/rc.local 
-
-systemctl start docker
+# 开机执行脚本，需要先赋予/etc/rc.d/rc.local执行权限
+# chmod +x /etc/rc.d/rc.local
+# echo $PWD/rc.sh >> /etc/rc.d/rc.local 
 
