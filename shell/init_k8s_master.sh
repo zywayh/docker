70a4d6f6ff5f8b8839447edccaf8257fb0f36e89
朱亚伟 <zhuyawei@prospect-media.cn>
2020-04-05 23:31:10 +0800

    修改命令

--- shell/init_k8s_master.sh
+++ shell/init_k8s_master.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "初始化前，必须将自己的ssh key公钥注册到自己的authorized_keys上"
+echo "生成公钥的命令：ssh-keygen -t rsa -C '邮箱'"
+echo "拷贝公钥到authorized_keys：echo '$(cat ~/.ssh/id_rsa.pub) >> authorized_keys'"
+
 # Master 节点初始化
 kubeadm init  \
 --image-repository registry.aliyuncs.com/google_containers \
